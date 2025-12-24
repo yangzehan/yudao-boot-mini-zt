d85fe0f0bb1821e96317a553b80509a74314e949
yzh <627617031@qq.com>
2025-12-24 17:31:26 +0800

    refactor(flink): 优化异步任务监控逻辑 - 添加了Flink JobStatusMessage的导入 - 重构了monitorClusters方法的循环逻辑 - 实现了对Flink作业状态的监控和处理 - 添加了异常处理机制以捕获集群访问错误 - 改进了集群关闭的触发条件和执行流程

--- yudao-module-flink/yudao-flink-common-deploy/src/main/java/cn/iocoder/yudao/module/flink/deploy/service/impl/AsyncTaskServiceImpl.java
+++ yudao-module-flink/yudao-flink-common-deploy/src/main/java/cn/iocoder/yudao/module/flink/deploy/service/impl/AsyncTaskServiceImpl.java
@@ -2,6 +2,7 @@ package cn.iocoder.yudao.module.flink.deploy.service.impl;
 
 import cn.iocoder.yudao.module.flink.deploy.service.AsyncTaskService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.runtime.client.JobStatusMessage;
 import org.apache.flink.runtime.minicluster.MiniCluster;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,19 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
   @Async
   @Override
   public void monitorClusters(MiniCluster miniCluster) {
-    while (true) {
-      if (!miniCluster.isRunning()) {
-        break;
+
+    while (!miniCluster.isRunning()) {
+      try {
+        for (JobStatusMessage jobStatusMessage : miniCluster.listJobs().get()) {
+          boolean isDone = jobStatusMessage.getJobState().isGloballyTerminalState();
+          if (isDone) {
+            miniCluster.close();
+            break;
+          }
+        }
+      } catch (Exception e) {
+        log.error("本地集群已经关闭或者无法访问", e);
       }
     }
-    log.info("MiniCluster停止执行");
   }
 }
